2ad1bd3f00042327dd984f9d3c6d0f411cbea527
Aditya Prakash Mishra <adityaprakashmishra.piemr@gmail.com>
2022-02-26 15:49:08 +0530

    pages before login

---
+++ aditya/src/components/SignIn.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const SignIn = () => {
+    return (
+        
+        <div>            
+           
+        
+        </div>
+        
+        
+        
+            
+            
+        
+    )
+};
